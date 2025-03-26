38fbfa0b68eb658134f77f45a02b739e1c4b25dd
maxierra <maxi.erramouspe77@gmail.com>
2025-03-26 17:12:22 -0300

    Simplificando configuración de puerto para Render

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,13 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: parseInt(process.env.VITE_PORT || '3000'),
     host: '0.0.0.0'
   },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-});
+})
